646ef3ddead85bfd024ca60a5eeb87432acf29b7
srpuce11 <arp.uce11.com>
2024-07-14 19:42:24 +0530

    sss

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Box, MenuItem, Select, FormControl, InputLabel } from "@mui/material";
 
-function HomePage() {
+const HomePage = ({ onSelectUser }) => {
+  const [users, setUsers] = useState([]);
+  const [selectedUserId, setSelectedUserId] = useState("");
+
+  useEffect(() => {
+    const fetchUsers = () => {
+      const keys = Object.keys(localStorage);
+      const userData = keys.map((key) => JSON.parse(localStorage.getItem(key)));
+      setUsers(userData);
+    };
+
+    fetchUsers();
+  }, []);
+
+  const handleUserChange = (event) => {
+    const userId = event.target.value;
+    setSelectedUserId(userId);
+    const selectedUser = users.find((user) => user.userId === userId);
+    onSelectUser(selectedUser);
+  };
+
   return (
-    <div>HomePage</div>
-  )
-}
+    <Box m="20px">
+      <FormControl fullWidth>
+        <InputLabel id="user-select-label">Select User</InputLabel>
+        <Select
+          labelId="user-select-label"
+          value={selectedUserId}
+          onChange={handleUserChange}
+        >
+          {users.map((user) => (
+            <MenuItem key={user?.userId} value={user?.userId}>
+              {user?.personalDetails?.firstName} {user?.personalDetails?.lastName}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </Box>
+  );
+};
 
-export default HomePage
+export default HomePage;
